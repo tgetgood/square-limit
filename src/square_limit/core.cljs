30ca3b9209e05442721dd86d19cc24d32b6f3878
Thomas Getgood <thomas.getgood@gmail.com>
2018-09-20 13:55:11 -0400

    We've reached the limit.

--- src/square_limit/core.cljs
+++ src/square_limit/core.cljs
@@ -8,91 +8,11 @@
 
 (def base
   (l/path
-   [(assoc l/bezier :to [-5 -100] :c1 [-3 -5] :c2 [-25 -90])
-    (assoc l/bezier :from [-5 -100] :to [-60 -200]
-           :c1 [-9 -105] :c2 [-61 -199])
-    (assoc l/line :from [-60 -200] :to [0 -250])]))
-
-(def b2
-  (l/path
    [(assoc l/line  :to [50 60])
     (assoc l/bezier :from [50 60] :to [150 15]
            :c1 [55 64] :c2 [101 54])
     (assoc l/bezier :from [150 15]  :to [250 0]
            :c1 [153 30] :c2 [220 10])]))
-
-;; (defn triangulate
-;;   "Given a path, creates a base tile with the appropriate symmetry.
-;;   Path is assumed to begin at the origin, and cover the bottom portion of the
-;;   tiling triangle (goes to the right)."
-;;   [path]
-;;   (let [[_ right-v] (l/endpoints path)
-;;         left-v      (rotc right-v)]
-;;     [path
-;;      (-> path
-;;          (l/reflect right-v [1 0])
-;;          (l/scale right-v 0.7071)
-;;          (l/rotate right-v -45))
-;;      (-> path
-;;          (l/rotate 90)
-;;          (l/reflect left-v [0 1])
-;;          (l/rotate left-v 45)
-;;          (l/scale left-v 0.7071))
-;;      (l/rotate path 90)]))
-
-;; (defn t [shape a b c depth]
-;;   (if (< 0 depth)
-;;     (let [o (lang/+ a b c)
-;;           mid (lang/+ a (lang/* 0.5 (lang/+ b c)))
-;;           diag (lang/- o mid)
-;;           p2 (lang/+ c a)
-;;           s1 (-> shape (l/scale p2 r2) (l/rotate p2 -45) (l/reflect mid diag))
-;;           ;s2 (-> shape (l/scale b r2) (l/rotate b 45) (l/reflect mid diag))
-;;           ]
-;;       (println a b c diag mid o)
-;;       [shape
-;;        (assoc l/line :style {:stroke :hotpink} :from mid :to o)
-;;        (assoc l/circle :radius 5 :centre mid)
-;;        (assoc l/circle :radius 5 :centre o :style {:stroke :none :fill :yellow})
-;;        (assoc l/circle :radius 5 :centre (lang/+ a b) :style {:stroke :none :fill :green})
-;;        s1
-;;        ;; (-> s1 (l/scale o r2) (l/rotate o -45) (l/reflect mid [0 1]))
-;;        (t s1 mid (lang/- b mid) (lang/- o mid) (dec depth))
-;;        #_(t s1 mid (lang/- p2 mid) (lang/- o mid) (dec depth))])
-;;     [])
-;;   )
-
-;; (defn syml [shape a b c]
-;;   (let [p (lang/+ b a)]
-;;     (-> shape
-;;         (l/scale p r2)
-;;         (l/rotate p 45)
-;;         (l/reflect (lang/+ a (lang/* 0.5 (lang/+ b c))) [0 1]))))
-
-;; (defn symr [shape a b c]
-;;   (let [p (lang/+ b a)]
-;;     (-> shape
-;;         (l/scale p r2)
-;;         (l/rotate p 45)
-;;         (l/reflect (lang/+ a (lang/* 0.5 (lang/+ b c))) (lang/+ a (lang/* 0.5 (lang/+ b c)))))))
-
-;; ;; REVIEW: Notice here that we're bleeding together aspects of the shape with
-;; ;; aspects of the frame. This is because even though f2 is derived geometrically
-;; ;; from f, it is a different kind of thing so far as the runtime is
-;; ;; concerned. You can ask a tile for its corners, but when you transform a tile,
-;; ;; that info gets lost. That's a big problem here.
-
-;; (def f (triangulate b2))
-
-;; (def picture
-;;   [f
-;;    (syml f [0 0] [0 250] [250 0])
-;;    (symr f [0 0] [0 250] [250 0])]
-;;   #_[(t f  [0 0] [0 250] [250 0] 5)
-;;    (l/with-style {:opacity 0.2 :stroke :blue}
-;;      (t (triangulate (assoc l/line :to [250 0])) [0 0] [0 250] [250 0] 8))
-;;    ])
-
 
 (def sqr2 (/ 1 (math/sqrt 2)))
 
@@ -102,70 +22,118 @@
 (defn triangulate [p]
   [p
    (-> p
-       #_(l/reflect :bottom-right [1 0])
        (l/scale :bottom-right sqr2)
-       #_(l/rotate :bottom-right -45)
-       #_(l/reflect :top-right [1 0])
-       #_(l/scale :bottom-right sqr2))
+       (l/rotate :bottom-right 45)
+       (l/reflect :bottom-right [1 0]))
    (l/rotate p 90)
    (-> p
        (l/rotate 90)
-       (l/scale :top-right sqr2)
-       (l/reflect :right [0 1])
-       (l/rotate :top-left 45))])
+       (l/scale :bottom-right sqr2)
+       (l/rotate :bottom-right -45)
+       (l/reflect :bottom-right [0 1]))])
 
-(def fish-outline (triangulate b2))
+(def fish-outline (triangulate base))
 
 (l/deftemplate fish
-  {:curve b2}
+  {:curve base}
   (triangulate curve)
   lang/Bounded
   (extent [this]
-    (let [{:keys [origin a]} (l/frame curve)]
-      (l/coordinate-frame origin [0 a] [a 0])))
+    (let [{:keys [origin a]} (l/frame curve)
+          w (first a)]
+      (l/coordinate-frame origin [w 0] [0 w])))
 
   lang/Affine
   (transform [this xform f]
     (Fish. (lang/transform curve xform f))))
 
-(defn above [a b]
-  [(l/scale b [1 0.5])
-   (-> a
-       (l/translate [:relative [0 0.5]])
-       (l/scale :bottom-left [1 0.5]))])
+(defn most [pick rf]
+  (fn [{:keys [origin a b]}]
+    (rf (pick origin) (reduce + (map pick [origin a b])))))
 
-(defn beside [a b]
-  [(l/scale a [0.5 1])
-   (-> b
-       (l/translate [:relative [0.5 0]])
-       (l/scale :bottom-right [0.5 1]))])
+(def left-most (most first min))
+(def right-most (most first max))
+(def top-most (most second max))
+(def bottom-most (most second min))
+
+(defn above
+  ([] [])
+  ([a b]
+   (let [delta (- (top-most (l/frame b)) (bottom-most (l/frame a)))]
+     [b (l/translate a [0 delta])
+      #_(l/with-style {:stroke :blue}
+          [(l/frame b) (l/frame (l/translate a [0 delta]))])])))
+
+(defn beside
+  ([] [])
+  ([a b]
+   (let [delta (- (right-most (l/frame a)) (left-most (l/frame b)))]
+     [(l/translate b [delta 0]) a
+      #_(l/with-style {:stroke :green}
+          [(l/frame b) (l/frame (l/translate a [delta 0]))])])))
+
+(defn matrix-grid
+  [m]
+  (reduce above
+          (map #(reduce beside %) m)))
 
 (defn q [a b c d]
-  (above (beside a b) (beside c d)))
+  (l/scale (matrix-grid [[a b] [c d]]) 0.5))
+
+(defn rot45 [shape]
+  (-> shape
+      (l/scale :top-left sqr2)
+      (l/rotate :top-left 45)))
 
 (def fish2
   (-> fish
-      (l/reflect :centre [0 1])
-      (l/rotate :top-right -45)
-      (l/scale [250 250] sqr2)
-))
+      rot45
+      (l/reflect :centre [0 1])))
 
-(def t
-  [fish
-   fish2
-   (l/rotate fish2 [125 125] 270)])
+(l/deftemplate t
+  {:shape fish}
+  (let [s2 (-> shape rot45 (l/reflect :centre [0 1]) )]
+    [shape
+     s2
+     (-> s2 (l/rotate :bottom-left 270))])
 
-(def u
-  (map #(l/rotate fish (* % 90)) (range 4)))
+  lang/Bounded
+  (extent [_] (l/frame shape)))
+
+(l/deftemplate u
+  {:shape fish2}
+  (map #(l/rotate shape :bottom-left (* % 90)) (range 4))
+
+  lang/Bounded
+  (extent [_]
+    ;; FIXME: You shouldn't have to set this by hand.
+    (l/coordinate-frame [0 0] [250 0] [0 250])))
+
+(def blank (l/style t {:stroke :none :opacity 0.3}))
+
+(def side1 (q blank blank (l/rotate t :centre 90) t))
+
+(def side2 (q side1 side1 (l/rotate t :centre 90) t))
+
+(defn cyc [shape]
+  (map #(l/rotate shape (* % 90)) (range 4)))
+
+(def v (cyc t))
+
+(def c1 (q blank blank blank u))
+
+(def c2 (q c1 side1 (l/rotate side1 :centre 90) u))
+
+(def square-limit2
+  (matrix-grid
+       [[c2 side2 (l/rotate c2 :centre 270)]
+        [(l/rotate side2 :centre 90) u (l/rotate side2 :centre 270)]
+        [(l/rotate c2 :centre 90) (l/rotate side2 :centre 180)
+         (l/rotate c2 :centre 180)]]))
 
 (def image
-  [(->  b2
-        triangulate
-        with-frames
-        ;; with-frames
-     (l/translate [300 300]))])
-
-
+  (-> square-limit2
+      (l/translate [200 200])))
 
 (defonce host (hosts/default-host {:size :fullscreen}))
 
